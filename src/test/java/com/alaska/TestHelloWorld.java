a3c16c3ee12ebb9647efecfcd175733fc332fef3
Laoshuatou-AlaSKa <752779922@qq.com>
2019-05-22 13:49:21 +0800

    2019-5-22 13:47 提交者：IDEA程序员 内容：新增java代码：HelloWorld.java       新增测试代码：TestHelloWorld.java

---
+++ src/test/java/com/alaska/TestHelloWorld.java
@@ -0,0 +1,20 @@
+package com.alaska;
+
+import org.junit.Test;
+
+/**
+ * 功能描述:
+ *
+ * @ClassName: TestHelloWorld
+ * Workstation:AlaSKa_
+ * @Author: Laoshuaitou-AlaSKa
+ * @Date: 2019/5/22 13:45
+ * @Version:0.1
+ */
+public class TestHelloWorld {
+	@Test
+	public void testHello(){
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.hello();
+	}
+}
