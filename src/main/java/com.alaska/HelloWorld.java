6e962bde942751dfb089a9996b56a626f03d4c65
Laoshuatou-AlaSKa <752779922@qq.com>
2019-05-22 13:49:21 +0800

    2019-5-22 14:00 提交者：SourceTree管理员 内容：额外提交

---
+++ src/main/java/com.alaska/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.alaska;
+
+/**
+ * 功能描述:
+ *
+ * @ClassName: HelloWorld
+ * Workstation:AlaSKa_
+ * @Author: Laoshuaitou-AlaSKa
+ * @Date: 2019/5/22 13:43
+ * @Version:0.1
+ */
+public class HelloWorld {
+	public void hello(){
+		System.out.println("HelloWorld");
+	}
+
+	public static void main(String[] args) {
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.hello();
+	}
+}
